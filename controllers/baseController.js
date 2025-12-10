a12ed765410f36878650e2ddaf39871cb17fb0b7
faftech3210 <franklinakor@gmail.com>
2025-12-10 23:33:33 +0100

    commit changes

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -11,5 +11,11 @@ baseController.buildHome = async function (req, res) {
   // req.flash("notice", "This is a flash message.")
   res.render("index", { title: "Home", nav })
 }
+/* ==================================
+ * Task 3 Trigger a 500 Server Error
+  ==================================*/
+// baseController.triggerError = async function (req, res, next) {
+//   throw new Error("500 Server Error")  
+// }
 
 module.exports = baseController
